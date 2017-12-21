ffcf77767363900904522a46c6cf35ddde58c0c6
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2017-12-21 15:30:47 -0500

    Holiday Gift Card

---
+++ OrcMathGUI/src/holidayCard/CardGUI.java
@@ -0,0 +1,26 @@
+package holidayCard;
+
+import guiTeacher.GUIApplication;
+
+public class CardGUI extends GUIApplication{
+
+	private static final long serialVersionUID = -2452328323352199392L;
+	
+	public CardGUI(int width, int height) {
+		super(width,height);
+		setVisible(true);
+	}
+
+	public static void main(String[] args){
+		CardGUI sample = new CardGUI(1247, 1148);
+		Thread go = new Thread(sample);
+		go.start();
+	}
+
+	@Override
+	public void initScreen() {
+		CardScreen s = new CardScreen(getWidth(), getHeight());
+		setScreen(s);		
+	}
+
+}
---
+++ OrcMathGUI/src/holidayCard/CardScreen.java
@@ -0,0 +1,41 @@
+package holidayCard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+import guiTeacher.components.Action;
+import guiTeacher.components.Button;
+import guiTeacher.components.Graphic;
+import guiTeacher.interfaces.Visible;
+import guiTeacher.userInterfaces.FullFunctionScreen;
+
+public class CardScreen extends FullFunctionScreen{
+
+	private Button next; 
+	private List<Visible> objects; 
+	
+	private static final long serialVersionUID = 1L;
+	
+	public CardScreen(int width, int height) {
+		super(width,height);
+	}
+
+	@Override
+	public void initAllObjects(List<Visible> viewObjects) {
+		objects = viewObjects;	
+		Graphic	image = new Graphic(0, 0, "resources/santa is a lie.jpg");
+		next = new Button(0,0,1247,1148,"", new Action() {
+			public void act() {
+				Graphic image2 = new Graphic(0,0,"resources/santa wrecking ball.jpg");
+				objects.add(image2);
+				objects.remove(image);
+			}
+		});
+		objects.add(next);
+		objects.add(image);
+	}
+
+}
