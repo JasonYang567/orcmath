ce6c170ee77067e2a9678fd65e3913313a3e0349
BT_1N3_16 <BT_1N3_16@BT_1N3_16.bktech.local>
2018-01-03 15:35:42 -0500

    Simon game

---
+++ OrcMathGUI/src/myStuff/Simon.java
@@ -0,0 +1,73 @@
+package myStuff;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+
+import guiTeacher.components.Button;
+import guiTeacher.components.TextArea;
+import guiTeacher.interfaces.Visible;
+import guiTeacher.userInterfaces.ClickableScreen;
+
+public class Simon extends ClickableScreen{
+
+	public Simon(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	private ArrayList<Color> move;
+	private Color[] colors= {Color.red,Color.blue,Color.green, Color.yellow};;
+	private TextArea numRounds;
+	private TextArea turn;
+	private TheButton red;
+	private TheButton blue;
+	private TheButton green;
+	private TheButton yellow;
+	private int round;
+	private int sequencelength;
+	private boolean pTurn;
+	
+
+
+	public void setUpButtons() {
+		red = new TheButton(100,100,Color.red,"Red");
+		blue = new TheButton(200,100,Color.blue,"Blue");
+		green = new TheButton(100,200,Color.green,"Green");
+		yellow = new TheButton(200,200,Color.yellow,"Yellow");
+	}
+	
+	public void setUpTextArea() {
+		numRounds = new TextArea(300,300,100,100,"Round: " + round + "\n Sequence Length: 3");
+		turn = new TextArea(500,300,100,100,"Simon's Turn");
+	}
+
+	@Override
+	public void initAllObjects(List<Visible> viewObjects) {
+		setUpButtons();
+		setUpTextArea();
+		viewObjects.add(red);
+		viewObjects.add(blue);
+		viewObjects.add(green);
+		viewObjects.add(yellow);
+		viewObjects.add(numRounds);
+		viewObjects.add(turn);
+	}
+	
+	public ArrayList<Color> sequence(){
+		move = new ArrayList<Color>();
+		while(move.size() < 3) {
+			int num = (int)(Math.random()*3);
+			if(move.size() > 1) {
+				for(Color c : move) {
+					if(c.equals(colors[num])) {
+						move.remove(c);
+					}
+				}
+			}			
+			move.add(colors[num]);
+		}
+		return move;
+	}
+	
+}
--- OrcMathGUI/src/myStuff/TheButton.java
+++ OrcMathGUI/src/myStuff/TheButton.java
@@ -9,37 +9,32 @@ import guiTeacher.components.Button;
 
 public class TheButton extends Button implements CustomLabel{
 
-	String s1;
-	String s2;
-	Color c;
+	private String s1;
+	private String s2;
+	private Color c;
 	
-	public TheButton(int x, int y) {
-		super(x,y,100,100,"Save",new Action(){
-
-			@Override
-			public void act() {
-				
-			}
-			
-		});
+	public TheButton(int x, int y,Color color,String name) {
+		super(x,y,100,100,name,color,null);
+		setIconColor(color);
 	}
 
 	@Override
 	public void updateString1(String string) {
-		
+		s1 = string;
 	}
 
 	@Override
 	public void updateString2(String string) {	
-		
+		s2 = string;
 	}
 
 	@Override
 	public void setIconColor(Color color) {
-		
+		c = color;
 	}
 
 	public void update(Graphics2D g) {
+		g.setColor(c);
 		
 	}
 
